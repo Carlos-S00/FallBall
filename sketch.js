acda6f626d26a56756c408c7d99788d670886364
Carlos-S00 <Carlos-S00@users.noreply.github.com>
2022-08-27 12:09:36 -0700

    Created functionality for floors having a beginning and ending position. Created a second floor demoonstrating the uniqueness of moveVars and the function

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let backgroundImage;
 let backgroundImageCopy;
 let gameScreenImage;
 let aspectRatio = 3/4;
+let heightOfGameScreen;
 
 let ballImage;
 let ballStartPosition = {x: .025, y: .05};
@@ -44,125 +45,125 @@ function onWindowResize() {
 
 
 class gameSystem{
-    constructor(aspectRatio){
-      this.recalcScreen();
-    }
-  
-    recalcScreen(){
-      this.widthOfGameScreen = aspectRatio * resolution.y;
-      this.heightOfGameScreen = resolution.y;
-  
-      this.position = {
-        startX: (resolution.x/2) - (this.widthOfGameScreen/2), 
-        startY: 0, 
-        endX: (resolution.x/2) + (this.widthOfGameScreen/2),
-        endY: resolution.y
-      }
-    }
-  
-    drawRainbowCanvas(){
-      console.log("ReCalculating")
-      let colorVal = 0;
-      let RBGColors = {r: 0, g: 0, b: 0};
-      for(let lineHeight = 1; lineHeight <= resolution.y; lineHeight += 2){
-        RBGColors = colorFunc(colorVal);
-        rainbowCanvas.stroke(RBGColors.r, RBGColors.g, RBGColors.b);
-        rainbowCanvas.line(0, lineHeight, resolution.x, lineHeight);
-        colorVal += 5;
-      }
-    }
+  constructor(aspectRatio){
+    this.recalcScreen();
+  }
 
-    displayGameScreen(){
-      image(gameScreenImage, this.position.startX, this.position.startY, this.widthOfGameScreen, this.heightOfGameScreen);
-    }
-  
-    resetImage(images){
-      return images.copy(backgroundImageCopy, 0, 0, backgroundImageCopy.width, backgroundImageCopy.height, 0, 0, backgroundImageCopy.width, backgroundImageCopy.height);
-    }
-    
-    displayGameBackground(){
-      let currentWidth = 139;
-      let currentHeight = 0;
+  recalcScreen(){
+    this.widthOfGameScreen = aspectRatio * resolution.y;
+    this.heightOfGameScreen = resolution.y;
 
-      brickCanvas.clear();
-      while(currentHeight <= this.position.endY){
-          
+    this.position = {
+      startX: (resolution.x/2) - (this.widthOfGameScreen/2), 
+      startY: 0, 
+      endX: (resolution.x/2) + (this.widthOfGameScreen/2),
+      endY: resolution.y
+    }
+  }
+
+  drawRainbowCanvas(){
+    console.log("ReCalculating")
+    let colorVal = 0;
+    let RBGColors = {r: 0, g: 0, b: 0};
+    for(let lineHeight = 1; lineHeight <= resolution.y; lineHeight += 2){
+      RBGColors = colorFunc(colorVal);
+      rainbowCanvas.stroke(RBGColors.r, RBGColors.g, RBGColors.b);
+      rainbowCanvas.line(0, lineHeight, resolution.x, lineHeight);
+      colorVal += 5;
+    }
+  }
+
+  displayGameScreen(){
+    image(gameScreenImage, this.position.startX, this.position.startY, this.widthOfGameScreen, this.heightOfGameScreen);
+  }
+
+  resetImage(images){
+    return images.copy(backgroundImageCopy, 0, 0, backgroundImageCopy.width, backgroundImageCopy.height, 0, 0, backgroundImageCopy.width, backgroundImageCopy.height);
+  }
+  
+  displayGameBackground(){
+    let currentWidth = 139;
+    let currentHeight = 0;
+
+    brickCanvas.clear();
+    while(currentHeight <= this.position.endY){
+        
+      brickCanvas.image(backgroundImage, this.position.startX - currentWidth, currentHeight, 140, 100, 58, 78, 140, 137);
+      currentWidth += 139;
+
+      while(this.position.startX - currentWidth > -139){
         brickCanvas.image(backgroundImage, this.position.startX - currentWidth, currentHeight, 140, 100, 58, 78, 140, 137);
         currentWidth += 139;
-
-        while(this.position.startX - currentWidth > -139){
-          brickCanvas.image(backgroundImage, this.position.startX - currentWidth, currentHeight, 140, 100, 58, 78, 140, 137);
-          currentWidth += 139;
-        }
-        currentWidth = 139;
-        currentHeight += 100 - 5;
       }
+      currentWidth = 139;
+      currentHeight += 100 - 5;
+    }
 
-      currentHeight = 0;
+    currentHeight = 0;
 
+    currentWidth = this.position.endX - 1;
+    while(currentHeight <= this.position.endY){
+      brickCanvas.image(backgroundImage, currentWidth, currentHeight, 140, 100, 58, 78, 140, 137);
+      
+      while(currentWidth < resolution.x ){
+        brickCanvas.image(backgroundImage, currentWidth + 139, currentHeight, 140, 100, 58, 78, 140, 137);
+        currentWidth += 139;
+      }
       currentWidth = this.position.endX - 1;
-      while(currentHeight <= this.position.endY){
-        brickCanvas.image(backgroundImage, currentWidth, currentHeight, 140, 100, 58, 78, 140, 137);
-        
-        while(currentWidth < resolution.x ){
-          brickCanvas.image(backgroundImage, currentWidth + 139, currentHeight, 140, 100, 58, 78, 140, 137);
-          currentWidth += 139;
-        }
-        currentWidth = this.position.endX - 1;
-        currentHeight += 100 - 5;
-      }
+      currentHeight += 100 - 5;
     }
-  
-    coordToScreen(position){
-      let Coordinate = {
-        x: this.position.startX + this.perToPx(position.x), 
-        y: this.position.startY + this.perToPx(position.y)};
-      return Coordinate;
-    }
-  
-    perToPx(percent){
-      return percent * this.widthOfGameScreen;
-    }
-  
-    displayWall(wall){
-      wall.colorNum = this.LineColor(wall.colorNum);
-      let startPosition = g.c(wall.topPosition);
-      let endPosition = g.c(wall.bottomPosition);
-  
-      line(startPosition.x, startPosition.y, endPosition.x, endPosition.y);
-    }
-    
-    displayFloor(floor){
-      let startPosition = g.c(floor.startPosition);
-      let endPosition = {x: floor.startPosition.x + floor.length, y: startPosition.y};
-      endPosition = g.c(endPosition);
+  }
 
-      erase();
-      line(startPosition.x, startPosition.y, endPosition.x, startPosition.y);
-      noErase();
-    }
+  coordToScreen(position){
+    let Coordinate = {
+      x: this.position.startX + this.perToPx(position.x), 
+      y: this.position.startY + this.perToPx(position.y)};
+    return Coordinate;
+  }
+
+  perToPx(percent){
+    return percent * this.widthOfGameScreen;
+  }
+
+  displayWall(wall){
+    wall.colorNum = this.LineColor(wall.colorNum);
+    let startPosition = g.c(wall.topPosition);
+    let endPosition = g.c(wall.bottomPosition);
+
+    line(startPosition.x, startPosition.y, endPosition.x, endPosition.y);
+  }
   
-    displayBall(ball){
-      let position = g.c(ball.BallPosition);
-      let ballDiameter = this.perToPx(ball.ballDiameter);
-  
-      translate(position.x, position.y);
+  displayFloor(floor){
+    let startPosition = g.c(floor.startPosition);
+    let endPosition = {x: floor.startPosition.x + floor.length, y: startPosition.y};
+    endPosition = g.c(endPosition);
+
+    erase();
+    line(startPosition.x, startPosition.y, endPosition.x, startPosition.y);
+    noErase();
+  }
+
+  displayBall(ball){
+    let position = g.c(ball.BallPosition);
+    let ballDiameter = this.perToPx(ball.ballDiameter);
+
+    translate(position.x, position.y);
+    rotate(ball.rotation);
+    image(ballImage, -(ballDiameter / 2), -(ballDiameter / 2), ballDiameter, ballDiameter);
+    resetMatrix();
+
+    if(position.x - (ballDiameter / 2) < this.position.startX){
+      translate(this.position.endX + (position.x -  this.position.startX), position.y);
       rotate(ball.rotation);
-      image(ballImage, -(ballDiameter / 2), -(ballDiameter / 2), ballDiameter, ballDiameter);
+      image(ballImage, -(ballDiameter / 2) , -(ballDiameter / 2), ballDiameter, ballDiameter);
       resetMatrix();
-  
-      if(position.x - (ballDiameter / 2) < this.position.startX){
-        translate(this.position.endX + (position.x -  this.position.startX), position.y);
-        rotate(ball.rotation);
-        image(ballImage, -(ballDiameter / 2) , -(ballDiameter / 2), ballDiameter, ballDiameter);
-        resetMatrix();
-      }else if(position.x + (ballDiameter / 2) > this.position.endX){
-        translate(this.position.startX - (this.position.endX - position.x), position.y);
-        rotate(ball.rotation);
-        image(ballImage,  -(ballDiameter / 2) , -(ballDiameter / 2), ballDiameter, ballDiameter);
-        resetMatrix();
-      }
+    }else if(position.x + (ballDiameter / 2) > this.position.endX){
+      translate(this.position.startX - (this.position.endX - position.x), position.y);
+      rotate(ball.rotation);
+      image(ballImage,  -(ballDiameter / 2) , -(ballDiameter / 2), ballDiameter, ballDiameter);
+      resetMatrix();
     }
+  }
 }
 
 class shortHand{
@@ -194,8 +195,22 @@ class floor{
   }
 
   moveHorizontally(){
-    if(this.moveVars.floorVelocity.x > 0  || this.moveVars.floorVelocity.x < 0){
+    if(this.moveVars.floorVelocity.x != 0){
       this.startPosition.x += this.moveVars.floorVelocity.x
+    }
+
+    if(this.moveVars.beginNEndPosition){
+      if(this.moveVars.floorVelocity.x > 0){
+        if(this.startPosition.x + this.length > this.moveVars.beginNEndPosition.end){ // If condition checks using the right edge of line
+          this.moveVars.floorVelocity.x *= -1;
+          this.startPosition.x = (this.moveVars.beginNEndPosition.end - ((this.startPosition.x + this.length) - this.moveVars.beginNEndPosition.end)) - this.length;
+        }
+      }else if(this.moveVars.floorVelocity.x < 0){
+        if(this.startPosition.x < this.moveVars.beginNEndPosition.begin){ // If condition checks using the left edge of line
+          this.moveVars.floorVelocity.x *= -1;
+          this.startPosition.x = this.moveVars.beginNEndPosition.begin + (this.moveVars.beginNEndPosition.begin - this.startPosition.x);
+        }
+      }
     }
   }
 
@@ -207,25 +222,18 @@ class floor{
 function colorFunc(colorNum){
   colorNum = colorNum % 1529;
   if(colorNum <= 255){ //0 - 255
-      stroke(255, colorNum, 0);
       return colors = {r: 255, g: colorNum, b: 0};
     }else if(colorNum <= 510){ // 256 - 510
-      stroke(510 - colorNum, 255, 0);
       return colors = {r: 510 - colorNum, g: 255, b: 0};
     }else if (colorNum <= 765){ // 511 - 765
-      stroke(0, 255, colorNum - 510);
       return colors = {r: 0, g: 255, b: colorNum - 510};
     }else if(colorNum <= 1020){ //766 - 1020
-      stroke(0, 1020 - colorNum, 255);
       return colors = {r: 0, g: 1020 - colorNum, b: 255};
     }else if(colorNum <= 1275){ // 1021 - 1275
-      stroke(colorNum - 1020, 0, 255);
       return colors = {r: colorNum - 1020, g: 0, b: 255};
     }else if(colorNum <= 1529){ //1276 - 1529
-      stroke(255, 0, 1529 - colorNum);
       return colors = {r: 255, g: 0, b: 1529 - colorNum};
     }else{
-      stroke(255, colorNum, 0);
       return colors = {r: 255, g: colorNum, b: 0};
     }
 }
@@ -239,61 +247,67 @@ function preload(){
 }
 
 function setup(){
-    rainbowCanvas = createGraphics(resolution.x, resolution.y);
-    window.theWholeCanvas = createCanvas(resolution.x, resolution.y);
-    brickCanvas = createGraphics(resolution.x, resolution.y);
+  rainbowCanvas = createGraphics(resolution.x, resolution.y);
+  window.theWholeCanvas = createCanvas(resolution.x, resolution.y);
+  brickCanvas = createGraphics(resolution.x, resolution.y);
 
-    rainbowCanvas.show();
-    brickCanvas.show();
-    rainbowCanvas.style("z-index", "-5");
-    brickCanvas.style("z-index", "3");
-    rainbowCanvas.style("position", "absolute");
-    brickCanvas.style("position", "absolute");
+  rainbowCanvas.show();
+  brickCanvas.show();
+  rainbowCanvas.style("z-index", "-5");
+  brickCanvas.style("z-index", "3");
+  rainbowCanvas.style("position", "absolute");
+  brickCanvas.style("position", "absolute");
 
-    let colorVal = 0;
-    let RBGColors = {r: 0, g: 0, b: 0};
-    for(let lineHeight = 1; lineHeight <= resolution.y; lineHeight += 2){
-      RBGColors = colorFunc(colorVal);
-      rainbowCanvas.stroke(RBGColors.r, RBGColors.g, RBGColors.b);
-      rainbowCanvas.line(0, lineHeight, resolution.x, lineHeight);
-      colorVal += 5;
+  angleMode(DEGREES);
+  textAlign(CENTER);
+  textSize(64);
+
+  game = new gameSystem(aspectRatio);
+  g = new shortHand(game);
+  heightOfGameScreen = game.heightOfGameScreen / game.widthOfGameScreen;
+
+  game.drawRainbowCanvas();
+  game.displayGameBackground();
+
+  let startPosition = {x: 0, y: heightOfGameScreen};
+  let floorVelocity = {x: 0.005, y: -.001};
+  let beginNEndPosition = {begin: 0.2, end: .8};
+  let moveVars = {floorVelocity: floorVelocity, beginNEndPosition: beginNEndPosition};
+  floors.push(new floor(startPosition, .2, moveVars, function(){
+    
+    this.moveVertically();
+
+    this.moveHorizontally();
+
+    if(this.startPosition.y < 0 || this.startPosition.y > heightOfGameScreen){
+      this.moveVars.floorVelocity.y *= -1;
     }
+  }));
 
-    angleMode(DEGREES);
-    textAlign(CENTER);
-    textSize(64);
-  
-    game = new gameSystem(aspectRatio);
-    g = new shortHand(game);
+  startPosition = {x: .2, y: heightOfGameScreen / 2};
+  floorVelocity = {x: 0.005, y: 0};
+  beginNEndPosition = {begin: 0.2, end: .8};
+  moveVars = {floorVelocity: floorVelocity, beginNEndPosition: beginNEndPosition};
+  floors.push(new floor(startPosition, .2, moveVars, function(){
+    
+    this.moveVertically();
 
-    game.displayGameBackground();
-
-    startPosition = {x: 0, y: game.heightOfGameScreen / game.widthOfGameScreen};
-    floorVelocity = {x: 0.005, y: -.001};
-    moveVars = {floorVelocity: floorVelocity};
-    floors.push(new floor(startPosition, .2, moveVars, function(){
-      
-      this.moveVertically();
-
-      this.moveHorizontally();
-
-      if(this.startPosition.x + this.length  > 1 || this.startPosition.x  < 0){
-        this.moveVars.floorVelocity.x *= -1;
-      }
-    }));
+    this.moveHorizontally();
+  }));
 }
 
 draw = function(){
+  if (frame == 360){
+      frame = 0;
+  }
+  background(colors.r, colors.g, colors.b);
 
-    if (frame == 360){
-        frame = 0;
-    }
-    background(colors.r, colors.g, colors.b);
+  game.displayGameScreen();
 
-    game.displayGameScreen();
+  for(let floorIndex = 0; floorIndex < floors.length; floorIndex++){
+    floors[floorIndex].doMovement();
+    game.displayFloor(floors[floorIndex]);
+  }
 
-    floors[0].doMovement();
-    game.displayFloor(floors[0])
-
-    frame++;
+  frame++;
 }
